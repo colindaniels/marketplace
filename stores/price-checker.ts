a3f6c0c94efb507561ce278f2368da9d8f2a2cef
Colin Daniels <colin.daniels@nandor.net>
2023-08-10 01:29:45 -0500

    removed selected_aspects

--- stores/price-checker.ts
+++ stores/price-checker.ts
@@ -4,7 +4,6 @@ export const usePriceCheckerStore = defineStore('priceChecker', {
       recommended_category: [],
       primary_categories: [],
       selected_category: {},
-      aspects: [],
-      selected_aspects: {}
+      aspects: []
     }),
   })
